c5c750595f14cc40453ef3a8eb12eef2f82fadc6
KiyoMinimum <kwwp.23@gmail.com>
2018-11-06 11:25:57 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// create the map
+var map = L.map('map', {
+    center: [40.75, -74.2],
+    zoom: 13,
+});
+
+// create the image
+var imageUrl = './image/nagomurucity.svg',
+    imageBounds = [[40.712216, -74.22655], [40.773941, -74.12544]];
+
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
